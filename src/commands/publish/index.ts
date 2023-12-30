bdd1eb2630bb5ed4ef3b80939ae954adbabfc1a0
shenmeng <shenmeng@dushu365.com>
2023-12-30 21:04:40 +0800

    feat: publish test

---
+++ src/commands/publish/index.ts
@@ -0,0 +1,45 @@
+// 发布npm包
+import { Args, Command, Flags } from '@oclif/core';
+import * as path from 'node:path';
+import * as fs from 'node:fs';
+import { execSync } from 'node:child_process';
+
+/**
+ * 发布npm包
+ * 1. 若为master分支打包正式版本
+ * 2. 其他打包为测试版本
+ */
+export default class Publish extends Command {
+    static description = 'publish project to npm , ';
+
+    static examples = ['fe publish'];
+
+    static flags = {
+        help: Flags.help({ char: 'h' }),
+    };
+
+    static args = {
+        person: Args.string({ description: 'Person to say hello to', required: true }),
+    };
+
+    async init() {
+        // 获取当前git分支
+        const currentGitName = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+        console.log('-- currentGitName', currentGitName);
+        if (currentGitName !== 'master') {
+            // execSync('npm publish --tag=beta');
+            const output = execSync('ls -l');
+            console.log(output.toString());
+        } else {
+            const output = execSync('ls -l');
+            console.log(output.toString());
+        }
+    }
+
+    async run(): Promise<void> {
+        await this.init();
+        const { args, flags } = await this.parse(Publish);
+
+        this.log(`publish ${args.person} from ${flags.from}! (./src/commands/hello/index.ts)`);
+    }
+}
